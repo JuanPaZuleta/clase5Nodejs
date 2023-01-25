1a6af82a8b2c95d1b5bb53d3f27af1dd39ff5730
Juan Pablo <juanpablozuleta@gmail.com>
2023-01-24 21:06:08 -0300

    Code for question.

---
+++ index.js
@@ -0,0 +1,18 @@
+import { inquireMenu, pausa } from "./helpers/inquirer.js";
+
+
+const main = async() => {
+    
+   let opt;
+   do{
+    opt = await inquireMenu();
+    
+    console.log({opt});
+
+    if (opt !== 0) await pausa(); 
+    
+   }
+   while(opt !== 0);
+}
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,18 @@
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+        //TODO leer BD si existe.
+    }
+
+    async ciudad(lugar = ''){
+        // petición http
+        console.log(lugar);
+
+        return []; //retornar lugares
+    }
+}
+
+export {
+    Busquedas
+}
